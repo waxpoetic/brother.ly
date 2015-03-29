8c22f52b47f984ac085f3aa7251c040672f17b25
Tom Scott <tscott@weblinc.com>
2015-03-29 01:50:58 -0400

    Clean up helpers and fix text

--- helpers/data_helper.rb
+++ helpers/data_helper.rb
@@ -5,20 +5,12 @@ module DataHelper
     Tilt['markdown'].new { source }.render
   end
 
-  def site_title
-    data.site.title || data.site.site
-  end
-
-  def site_description
-    data.site.description
-  end
-
   def site_url
     data.site.url
   end
 
   def open_signups?
-    data.site.splash_text.present?
+    data.site.splash_text.blank?
   end
 
   def live_event?
